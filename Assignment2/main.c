e3147a91d0c5a2f400da330fd44a96ae49c573fd
theGANOUSH <babaganoush45@gmail.com>
2015-10-12 19:11:49 -0400

    Added main.c

---
+++ Assignment2/main.c
@@ -0,0 +1,27 @@
+/*
+Author: Austin Lowe & Kyle Scorcia
+Date: 10/12/2015
+Assignment 2: PL/0 Scanner
+COP3402 Systems Software
+Matthew B. Gerber (Adjunct)
+University of Central Florida
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+nulsym = 1, identsym, numbersym, plussym, minussym,
+multsym, slashsym, oddsym, eqsym, neqsym, lessym, leqsym,
+gtrsym, geqsym, lparentsym, rparentsym, commasym, semicolonsym,
+periodsym, becomessym, beginsym, endsym, ifsym, thensym,
+whilesym, dosym, callsym, constsym, varsym, procsym, writesym,
+readsym, elsesym
+} token_type;
+
+int main()
+{
+
+    return 0;
+}

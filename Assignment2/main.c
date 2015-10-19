246ea52d828834e2f83bf9ebbc3caad2532948c9
theGANOUSH <babaganoush45@gmail.com>
2015-10-19 00:00:31 -0400

    Implemented Clean Input File
    Will create a input file that will be stripped of all comments.

--- Assignment2/main.c
+++ Assignment2/main.c
@@ -12,7 +12,12 @@ University of Central Florida
 #include <string.h>
 #include <ctype.h>
 
-FILE *ifp, *lfp, *cifp, tlfp*;
+#define MAX_INT_SIZE 5
+#define MAX_IDENT_SIZE 11
+
+FILE *ifp, *lfp, *cifp, *tlfp;
+
+int HALT = 0;
 
 
 typedef enum {
@@ -24,12 +29,96 @@ whilesym, dosym, callsym, constsym, varsym, procsym, writesym,
 readsym, elsesym
 } token_type;
 
+struct lexeme
+{
+    int type;
+    char *identifier;
+    struct lexeme *next;
+};
+
+char removeComment(char a);
+char removeComment(char a)
+{
+    if(a == '/')
+    {
+        return fgetc(ifp);
+    }
+    else
+    {
+        return removeComment(fgetc(ifp));
+    }
+
+}
+
+void getCleanInput();
+void getCleanInput()
+{
+    char a, b;
+    while(a != EOF)
+    {
+        a = fgetc(ifp);
+
+
+        if(a == '/')
+        {
+            b = fgetc(ifp);
+
+            if(b == '*')
+            {
+                a = removeComment(b);
+            }
+        }
+
+        if(a != EOF)
+        {
+            fprintf(cifp, "%c", a);
+        }
+
+    }
+}
+
+
 int main()
 {
+    char b = '\0';
+
     ifp = fopen("input.txt", "r");
-    lfp = fopen("lexemetable.txt", "w");
-    cifp = fopen("cleaninput.txt", "w");
-    tlfp = fopen("tokenlist.txt", "w");
+    lfp = fopen("lexemetable.txt", "w+");
+    cifp = fopen("cleaninput.txt", "w+");
+    tlfp = fopen("tokenlist.txt", "w+");
+
+    fprintf(lfp, "LEXEME\tTOKEN TYPE\n");
+
+    getCleanInput();
+
+    rewind(cifp);
+
+    b = fgetc(cifp);
+
+    while(b != EOF)
+    {
+        char nDigit;
+        if(isdigit(b))
+        {
+            nDigit = fgetc(cifp);
+            printf("%c", b);
+
+            while(isdigit(nDigit))
+            {
+                printf("%c", nDigit);
+                nDigit = fgetc(cifp);
+
+            }
+            printf("\n");
+        }
+
+        b = fgetc(cifp);
+    }
+
+    fclose(ifp);
+    fclose(lfp);
+    fclose(cifp);
+    fclose(tlfp);
 
     return 0;
 }

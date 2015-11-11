4a5fc78adb92c1b14bb0f2b58e11dd97b73b1e80
theGANOUSH <babaganoush45@gmail.com>
2015-11-11 14:52:06 -0500

    Implemented parsing functions
    Added program(), getToken(), getIdentifier().

--- Assignment3/main.c
+++ Assignment3/main.c
@@ -12,45 +12,55 @@ University of Central Florida
 #include <string.h>
 #include <ctype.h>
 
+#define MAX_SYMBOL_TABLE_SIZE 100
+
 FILE *ifp;
 FILE *ofp;
 FILE *stfp;
 
-struct token
+int currentToken = 0;
+int totalSymbols = 0;
+int lLevel = 0;
+
+typedef enum
 {
-    int line;
-    char tokenString[11];
-    struct token *next;
-};
+    nulsym = 1, identsym, numbersym, plussym, minussym, multsym, slashsym, oddsym,
+    eqsym, neqsym, lessym, leqsym, gtrsym, geqsym, lparentsym, rparentsym, commasym,
+    semicolonsym, periodsym, becomessym, beginsym, endsym, ifsym, thensym,  whilesym,
+    dosym, callsym, constsym, varsym, procsym, writesym, readsym, elsesym
+} token_type;
+
+typedef struct symbol
+ {
+    int kind;
+    char name[12];
+    int val;
+    int level;
+    int addr;
+} symbol;
+
+symbol symbol_table[MAX_SYMBOL_TABLE_SIZE];
+
+void getToken();
+void program();
+void block();
+void getIdentifier(symbol node);
+
 
 int main()
 {
-    struct token *root = NULL;
-    
-    int temp;
 
     ifp = fopen("tokenlist.txt", "r+");
     ofp = fopen("mcode.txt", "w+");
     stfp = fopen("symboletable.txt", "w+");
-    
-    
-    while(fscanf(ifp, "%d", &temp) != EOF)
+
+    if(ifp == NULL)
     {
-    	switch(temp)
-    	{
-    		case identsym:
-    		break;
-    		
-    		case numbsym:
-    		break;
-    		
-    		default:
-    		break;
-    		
-    	}
-    	
-    	
+        printf("INPUT FILE IS NOT VALID\nEXITING");
+
+        return -1;
     }
+
 
 
 
@@ -59,4 +69,107 @@ int main()
     fclose(stfp);
 	return 0;
 }
+
+void getToken()
+{
+    fscanf(ifp, "%d", &currentToken);
+
+}
+
+void getIdentifier(symbol node)
+{
+    fscanf(ifp, "%s", node.name);
+}
+void statement()
+{
+    getToken();
+    if(currentToken != becomessym)
+    {
+        printf("ERROR");
+        //Halt
+    }
+
+}
+void block()
+{
+    if(currentToken == constsym)
+    {
+        getToken();
+
+        while(currentToken == commasym)
+        {
+            symbol temp;
+            getToken();
+
+            if(currentToken != identsym)
+            {
+                printf("ERROR");
+
+            }
+            temp.kind = 1;
+            getIdentifier(temp);
+            temp.level = lLevel;
+            temp.addr = -1;
+
+            getToken();
+
+            if(currentToken != eqsym)
+            {
+                printf("ERROR");
+
+            }
+            getToken();
+
+            if(currentToken != numbersym)
+            {
+                printf("ERROR");
+
+            }
+
+            getToken();
+            temp.val = currentToken;
+            symbol_table[totalSymbols++] = temp;
+
+            getToken();
+
+        }
+
+    }
+
+    if(currentToken == varsym)
+    {
+        while(currentToken == commasym)
+        {
+            getToken();
+
+            if(currentToken !=identsym)
+            {
+                printf("ERROR");
+                //HALT
+
+            }
+
+
+        }
+    }
+
+}
+
+void program()
+{
+    getToken();
+
+    block();
+
+    if(currentToken != periodsym)
+    {
+        printf("ERROR");
+        //halt
+    }
+
+}
+
+
+
+
 

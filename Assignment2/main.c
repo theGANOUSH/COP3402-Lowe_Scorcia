fdf63fd1b31d1c05c588250c2ba7dee2c8c83889
KickSave35 <kscorcia@knights.ucf.edu>
2015-11-30 22:22:11 -0500

    Added Screenshots of Scanner & Parser error
    .

--- Assignment2/main.c
+++ Assignment2/main.c
@@ -12,7 +12,6 @@ University of Central Florida
 #include <stdlib.h>
 #include <ctype.h>
 
-<<<<<<< HEAD
 #define MAX_SYMBOL_TABLE_SIZE 100
 #define MAX_IDENTIFIER_LENGTH 11
 #define MAX_NUMBER_LENGTH 5
@@ -25,14 +24,6 @@ typedef struct{
     int level;          // L level
     int addr;           // M level
 } symbol;
-=======
-#define MAX_INT_SIZE 5
-#define MAX_IDENT_SIZE 11
-
-FILE *ifp, *lfp, *cifp, *tlfp;
-
-int HALT = 0;
->>>>>>> origin/master
 
 
 //A node that represents a single word or symbol in the program code
@@ -53,99 +44,6 @@ typedef enum{
     readsym , elsesym
 } token_type;
 
-<<<<<<< HEAD
-=======
-struct lexeme
-{
-    int type;
-    char *identifier;
-    struct lexeme *next;
-};
-
-char removeComment(char a);
-char removeComment(char a)
-{
-    if(a == '/')
-    {
-        return fgetc(ifp);
-    }
-    else
-    {
-        return removeComment(fgetc(ifp));
-    }
-
-}
-
-void getCleanInput();
-void getCleanInput()
-{
-    char a, b;
-    while(a != EOF)
-    {
-        a = fgetc(ifp);
-
-
-        if(a == '/')
-        {
-            b = fgetc(ifp);
-
-            if(b == '*')
-            {
-                a = removeComment(b);
-            }
-        }
-
-        if(a != EOF)
-        {
-            fprintf(cifp, "%c", a);
-        }
-
-    }
-}
-
-
-int main()
-{
-    char b = '\0';
-
-    ifp = fopen("input.txt", "r");
-    lfp = fopen("lexemetable.txt", "w+");
-    cifp = fopen("cleaninput.txt", "w+");
-    tlfp = fopen("tokenlist.txt", "w+");
-
-    fprintf(lfp, "LEXEME\tTOKEN TYPE\n");
-
-    getCleanInput();
-
-    rewind(cifp);
-
-    b = fgetc(cifp);
-
-    while(b != EOF)
-    {
-        char nDigit;
-        if(isdigit(b))
-        {
-            nDigit = fgetc(cifp);
-            printf("%c", b);
-
-            while(isdigit(nDigit))
-            {
-                printf("%c", nDigit);
-                nDigit = fgetc(cifp);
-
-            }
-            printf("\n");
-        }
-
-        b = fgetc(cifp);
-    }
-
-    fclose(ifp);
-    fclose(lfp);
-    fclose(cifp);
-    fclose(tlfp);
->>>>>>> origin/master
 
 //Function declarations
 node *BeginsWithLetter(char firstLetter, FILE *inputFile, node* tail, FILE *outputFile);
